85a09b568dc9436317b08695f05ff6c3320b089d
Bardur Arantsson <bardur@scientician.net>
2021-02-18 23:08:41 +0100

    Update copyright blurbs

--- client/src/Database/PostgreSQL/Harness/Client.hs
+++ client/src/Database/PostgreSQL/Harness/Client.hs
@@ -1,5 +1,5 @@
 {-
-    Copyright (c) 2015, Bardur Arantsson
+    Copyright (c) 2015-2020 Bardur Arantsson
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
